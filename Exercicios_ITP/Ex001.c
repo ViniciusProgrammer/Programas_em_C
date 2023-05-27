535b99bc0aedc7f4dc5ce7f2a5bffcd55a9c3164
Vinicius Davi <vinicios.david.083@ufrn.edu.br>
2023-05-27 20:46:02 -0300

    Questão resolvida

--- Exercicios_ITP/Ex001.c
+++ Exercicios_ITP/Ex001.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
 int main(){
-    printf("Vinicios\n");
+    int i = 0;
+    char palavra[60];
     
+    scanf("%60[^\n]", palavra);
+
+    palavra[i] = toupper(palavra[i]);
+
+    while(palavra[i] != '\0'){
+        if(palavra[i] == ' '){
+            palavra[i+1] = toupper(palavra[i + 1]);
+        }
+        i++;
+    }
+
+    printf("%s\n", palavra);
+
     return 0;
 }
